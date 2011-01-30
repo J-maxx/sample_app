347063b9faaf99d56d4b96e55e4a957c4bb33b75
Jeff Roth <jeff_roth71@mac.com>
2011-01-30 14:57:22 -0500

    Added a Users controller and layout link tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 SampleApp::Application.routes.draw do
   
+  get "users/new"
+
   root :to => "pages#home"
   
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'  
   match '/amante',  :to => 'pages#amante' 
+  match '/signup',  :to => 'users#new' 
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -11,7 +11,7 @@ require 'spec_helper'
     get '/contact'
     response.should have_selector('title', :content => "Contact")
   end
-  
+    
     it "Should have an about page at '/about'" do
     get '/about'
     response.should have_selector('title', :content => "About")
@@ -20,6 +20,25 @@ require 'spec_helper'
     it "Should have a help page at '/help'" do 
     get '/help'
     response.should have_selector('title', :content => "Help")
-end
-
+  end
+  
+    it "Should have a signup page at '/signup'" do
+      get '/signup'
+      response.should have_selector('title', :content => 'Sign Up')
+    end
+    
+    it "Should have the right links on the layout" do
+      visit root_path
+      response.should have_selector('title', :content => 'Home')
+      click_link 'About'
+      response.should have_selector('title', :content => 'About')
+      click_link 'Contact'
+      response.should have_selector('title', :content => 'Contact')
+      click_link 'Home'
+      response.should have_selector('title', :content => 'Home')
+      click_link 'Sign Up Now'
+      response.should have_selector('title', :content => 'Sign Up')
+      response.should have_selector('a[href="/"]>img')
+    end
+    
 end
