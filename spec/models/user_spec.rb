09dd9d2653ef36c89652b1450d886e945daa3f83
Jeff Roth <jeff_roth71@mac.com>
2011-02-03 09:09:10 -0500

    Finished password machinery

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -43,7 +43,7 @@ require 'spec_helper'
       addresses.each do |address|
       invalid_email_user=User.new(@attr.merge(:email => address))
       invalid_email_user.should_not be_valid
-    end
+    end  
   end
   
     it "Should reject duplicate email addresses" do
@@ -106,7 +106,53 @@ require 'spec_helper'
     end
     
     it "should have encrypted password attribute" do
-      @user.should respond_to(:encrypted_password )
+      @user.should respond_to(:encrypted_password)
+    end
+    
+    it "should set the encrypted password attribute" do
+      @user.encrypted_password.should_not be_blank
+    end 
+  
+    it "should have a salt" do
+      @user.should respond_to(:salt)
+    end
+  end
+  
+  describe "has password? method" do
+   
+    before(:each) do
+      @user=User.create!(@attr)
+    end
+   
+    it "should exist" do
+      @user.should respond_to(:has_password?)
+    end
+    
+    it "should return true if the passwords match" do
+      @user.has_password?(@attr[:password]).should be_true
+    end
+    
+    it "should return false if the passwords don't match" do
+      @user.has_password?("invalid").should be_false
+    end
+  end
+  
+  describe "autheticate method" do
+      
+    it "should exist" do
+      User.should respond_to(:authenticate)
+    end
+    
+    it "should return nil on email/password mismatch" do
+      User.authenticate(@attr[:email], "wrongpass").should be_nil
+    end
+    
+    it "should return nil for an email address with no user" do
+      User.authenticate("bar@foo.com", @attr[:password]).should be_nil
+    end
+    
+    it "should return the user on an email/password match" do
+      User.authenticate(@attr[:password], @attr[:email]).should == @user
     end
   end
 end
