e465bdd07fd168fbe2f254ca3fd5732c2f6cf408
Jeff Roth <jeff_roth71@mac.com>
2011-02-14 21:34:49 -0500

    Friendly forwarding and correct user requirement

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_filter :authenticate, :only => [:edit, :update]
+  before_filter :correct_user, :only => [:edit, :update]
   
     def show
       @user = User.find(params[:id])
@@ -44,7 +45,9 @@ class UsersController < ApplicationController
       deny_access unless signed_in?
     end
     
-    def deny_access
-      redirect_to signin_path, :notice => "Please sign in to access this page."
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
     end
+    
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -26,6 +26,24 @@ module SessionsHelper
     self.current_user = nil  
   end
 
+  def deny_access
+    store_location
+    redirect_to signin_path, :notice => "Please sign in to access this page."
+  end
+
+  def store_location
+      session[:return_to] = request.fullpath
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    clear_return_to
+  end
+
+  def clear_return_to
+    session[:return_to] = nil
+  end
+  
   private
   
   def user_from_remember_token
@@ -36,7 +54,4 @@ module SessionsHelper
     cookies.signed[:remember_token] || [nil, nil]
   end
   
-  def deny_access
-    redirect_to signin_path, :notice => "Please sign in to access this page."
-  end
 end
